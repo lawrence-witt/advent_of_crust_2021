1601407a69f1aa73d781918c3c9e1197dfda70e0
lawrence-witt <lawrence.witt@hotmail.co.uk>
2022-11-01 23:33:59 +0000

    Read input.txt

--- day-1/bin/part-1.rs
+++ day-1/bin/part-1.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
+fn read_input() -> Vec<String> {
+    return fs::read_to_string("day-1/input.txt")
+        .expect("failed to read file path")
+        .trim()
+        .split("\r\n")
+        .map(String::from)
+        .collect();
+}
+
 fn main() {
-    println!("Hello World!")
+    let input = read_input();
+    println!("{:?}", input);
 }
