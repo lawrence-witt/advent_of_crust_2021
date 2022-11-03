c73b8bf2b0b7517055cbc299814091406502e707
Lawrence Witt <55201111+lawrence-witt@users.noreply.github.com>
2022-11-03 00:10:15 +0000

    Day 1 (#1)

---
+++ Makefile
@@ -0,0 +1,5 @@
+day1_part1: 
+	cargo run --manifest-path day-1/Cargo.toml --bin part-1
+
+day1_part2: 
+	cargo run --manifest-path day-1/Cargo.toml --bin part-2
---
+++ day-1/src/part-1.rs
@@ -0,0 +1,19 @@
+use common;
+
+fn str_to_i32(value: &str) -> i32 {
+    return value.parse::<i32>().unwrap();
+}
+
+fn main() {
+    let input = common::read_rn_input(1);
+    let mut count = 0;
+    for n in 0..input.len() {
+        if n == 0 {
+            continue;
+        }
+        if str_to_i32(&input[n]) > str_to_i32(&input[n - 1]) {
+            count += 1;
+        }
+    }
+    println!("{}", count);
+}
