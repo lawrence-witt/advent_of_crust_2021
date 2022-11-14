6c39c194931126bd6999d037fb76a842df3fd869
Lawrence Witt <55201111+lawrence-witt@users.noreply.github.com>
2022-11-14 23:02:00 +0000

    Generate script (#3)

--- day-1/src/part-2.rs
+++ day-1/src/part-2.rs
@@ -1,7 +1,7 @@
-use common::*;
+extern crate common;
 
 fn str_arr_to_i32_vec(value: &[String]) -> Vec<i32> {
-    return value.iter().map(|v| str_to_i32(v)).collect();
+    return value.iter().map(|v| common::str_to_i32(v)).collect();
 }
 
 fn sum_i32_vec(value: &Vec<i32>) -> i32 {
@@ -9,7 +9,7 @@ fn sum_i32_vec(value: &Vec<i32>) -> i32 {
 }
 
 fn main() {
-    let input = read_new_line_input(1);
+    let input = common::read_new_line_input(1);
     let mut count = 0;
     for n in 0..input.len() {
         if n < 3 {
--- day-2/src/part-2.rs
+++ day-2/src/part-2.rs
@@ -1,22 +1,22 @@
-use common::*;
+extern crate common;
 
 fn main() {
-    let input = read_new_line_input(2);
+    let input = common::read_new_line_input(2);
     let mut horizontal = 0;
     let mut depth = 0;
     let mut aim = 0;
     for i in input.iter() {
         let cmd: Vec<&str> = i.split_whitespace().collect();
         let dir = cmd[0];
-        let v = str_to_i32(cmd[1]);
+        let v = common::str_to_i32(cmd[1]);
         match dir {
             "forward" => {
                 horizontal += v;
                 depth += v * aim;
-            },
+            }
             "up" => aim -= v,
             "down" => aim += v,
-            _ => panic!("invalid direction {}", dir)
+            _ => panic!("invalid direction {}", dir),
         }
     }
     println!("{}", horizontal * depth);
