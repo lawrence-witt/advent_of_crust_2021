bd95e8c620dc78fc9c223f316bb5333c0c37edc0
Lawrence Witt <55201111+lawrence-witt@users.noreply.github.com>
2022-11-24 23:02:34 +0000

    Day 7 (#8)

---
+++ day-7/src/part-2.rs
@@ -0,0 +1,12 @@
+extern crate common;
+mod utils;
+
+fn main() {
+    let input = common::read_input(7, "\n");
+    let displays = utils::format_input(input);
+    let mut result: u64 = 0;
+    for display in displays {
+      result += u64::from(display.solve())
+    }
+    println!("{:#?}", result)
+}
