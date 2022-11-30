37596c9fa573d2bd1568e368d73626aa48489ddc
Lawrence Witt <lawrence.witt@hotmail.co.uk>
2022-11-30 09:23:05 +0000

    Complete part 2

--- day-13/src/part-1.rs
+++ day-13/src/part-1.rs
@@ -1,20 +1,10 @@
-use std::collections::HashMap;
-
 mod utils;
 
 fn main() {
-    let (mut points, folds) = utils::get_input();
+    let (mut points, folds, _, _) = utils::get_input();
     for p in points.iter_mut() {
         p.fold(&folds.get(0).unwrap());
     }
-    let mut map: HashMap<String, i32> = Default::default();
-    for p in points {
-        let p_str = p.to_string();
-        while None == map.get(&p_str) {
-            map.insert(String::from(&p_str), 0);
-        }
-        let p_count = map.get(&p_str).unwrap();
-        map.insert(String::from(&p_str), p_count + 1);
-    }
-    println!("{:#?}", map.keys().len());
+    let unique_points = utils::get_unique_points(&points);
+    println!("{:#?}", unique_points.len());
 }
--- day-13/src/utils.rs
+++ day-13/src/utils.rs
@@ -1,13 +1,18 @@
-pub fn get_input() -> (Vec<Point>, Vec<Fold>) {
+use std::collections::HashMap;
+
+pub fn get_input() -> (Vec<Point>, Vec<Fold>, u16, u16) {
     let input = common::read_input(13, "\n\n");
     let mut points: Vec<Point> = Default::default();
     let mut folds: Vec<Fold> = Default::default();
+    let mut x: u16 = 0;
+    let mut y: u16 = 0;
     for p in input.get(0).unwrap().split("\n").collect::<Vec<&str>>() {
-        let xy: Vec<&str> = p.split(",").collect();
-        points.push(Point{
-            x: xy.get(0).unwrap().parse::<u16>().unwrap(), 
-            y: xy.get(1).unwrap().parse::<u16>().unwrap()
-        })
+        let pxy: Vec<&str> = p.split(",").collect();
+        let px = pxy.get(0).unwrap().parse::<u16>().unwrap();
+        let py = pxy.get(1).unwrap().parse::<u16>().unwrap();
+        x = std::cmp::max(x, px);
+        y = std::cmp::max(y, py);
+        points.push(Point{ x: px, y: py })
     }
     let exp = regex::Regex::new(r"(?P<axis>x|y)=(?P<value>\d+)").unwrap();
     for f in input.get(1).unwrap().split("\n").collect::<Vec<&str>>() {
@@ -21,34 +26,47 @@ pub fn get_input() -> (Vec<Point>, Vec<Fold>) {
             folds.push(Fold { axis, value: u16::from(*value) })
         }
     }
-    return (points, folds)
+    return (points, folds, x, y)
 }
 
-#[derive(Debug)]
+pub fn get_unique_points(points: &Vec<Point>) -> Vec<Point> {
+    let mut map: HashMap<String, Point> = Default::default();
+    for p in points {
+        let p_str = p.to_string();
+        if None == map.get(&p_str) {
+            map.insert(String::from(&p_str), p.clone());
+        }
+    }
+    return map.into_values().collect();
+}
+
+#[derive(Debug, PartialEq)]
 pub struct Point {
-    x: u16,
-    y: u16
+    pub x: u16,
+    pub y: u16
 }
 
 impl Point {
     pub fn fold(&mut self, fold: &Fold) {
         if fold.axis == Axis::X {
             if self.x > fold.value {
-                self.x -= fold.value
+                self.x -= fold.value + 1;
             } else {
-                self.x = fold.value - self.x
+                self.x = fold.value - self.x - 1;
             }
         } else {
             if self.y > fold.value {
-                self.y -= fold.value
-            } else {
-                self.y = fold.value - self.y
+                self.y = fold.value - (self.y - fold.value);
             }
         }
     }
 
     pub fn to_string(&self) -> String {
         return format!("{}-{}", self.x, self.y);
+    }
+
+    pub fn clone(&self) -> Point {
+        return Point{x: self.x, y: self.y}
     }
 }
 
@@ -60,6 +78,6 @@ pub enum Axis {
 
 #[derive(Debug)]
 pub struct Fold {
-    axis: Axis,
-    value: u16
+    pub axis: Axis,
+    pub value: u16
 }
